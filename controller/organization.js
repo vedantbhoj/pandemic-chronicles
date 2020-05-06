0be1b263100810ff4d3eafe064d288e471318e1f
arpitsharrma <arpit.sharma@sjsu.edu>
2020-05-06 11:21:13 -0700

    Added Organization data flow from MongoDB

--- controller/organization.js
+++ controller/organization.js
@@ -1,15 +1,41 @@
+var Organization = require("../models/organization.js");
+
 module.exports.who = function(req, res) {
-  res.send();
+  Organization.find({id:1}).exec(function(err, organization) {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      res.render("./organization", { organization: organization });
+    }
+  });
 };
 
 module.exports.cdc = function(req, res) {
-  res.send();
+  Organization.find({id:2}).exec(function(err, organization) {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      res.render("./organization", { organization: organization });
+    }
+  });
 };
 
 module.exports.unicef = function(req, res) {
-  res.send();
+  Organization.find({id:3}).exec(function(err, organization) {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      res.render("./organization", { organization: organization });
+    }
+  });
 };
 
 module.exports.worldbank = function(req, res) {
-  res.send();
+  Organization.find({id:4}).exec(function(err, organization) {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      res.render("./organization", { organization: organization });
+    }
+  });
 };
